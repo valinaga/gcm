8690dc70e62e4ca810c945e75e7217caf6466875
Vali Nagacevschi <vali@experiment.ro>
2012-08-11 09:59:06 +0300

    ok

---
+++ lib/gcm.rb
@@ -0,0 +1,58 @@
+require 'httparty'
+require 'cgi'
+
+class GCM
+  include HTTParty
+  format :json
+  default_timeout 30
+
+  attr_accessor :timeout, :auth_key
+
+  PUSH_URL = 'https://android.apis.google.com/c2dm/send'
+
+  class << self
+    attr_accessor :auth_key
+
+    def send_notifications(notifications = [], auth_key = @auth_key)
+      gcm = GCM.new(auth_key)
+      notifications.collect do |notification|
+        registration_id = notifiction.delete(:registration_id)
+        notification.merge!({ :registration_ids => [ registration_id ] }) unless registration_id.nil?
+        gcm.send_notification(notification)
+      end
+    end
+    
+  end
+
+  def initialize(auth_key = nil)
+    @auth_key = auth_key || self.class.auth_key
+  end
+
+  # {
+  #   :registration_ids => ["...", "..."],
+  #   :data => {
+  #     :message => "Hi!", 
+  #     :score => 7
+  #   },
+  #   :collapse_key => "optional collapse_key string"
+  # }
+  def send_notification(options)
+    options[:collapse_key] ||= 'foo'
+    params = {
+      :body    => options.to_json,
+      :headers => {
+        'Content-Type' => 'application/json',
+        'Authorization' => "key=#{@auth_key}"
+      }
+    }
+
+    response = self.class.post(PUSH_URL, params)
+    puts response
+    # check for authentication failures
+    # raise response.parsed_response if response['Error=']
+
+    #@auth_token = response.body.split("\n")[2].gsub('Auth=', '')
+
+  end
+
+end
